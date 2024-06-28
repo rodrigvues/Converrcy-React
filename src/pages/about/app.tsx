353c7cbe64c6cfe15213743e5078a1e4d6be4d55
Vitor Rodrigues <vitor.rodriguess3112@gmail.com>
2024-06-27 23:14:13 -0300

    organizing structure for pages

---
+++ src/pages/about/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About: React.FC = () => {
+    return (
+      <div id="About">
+        
+      </div>
+    );
+  }
+  
+  export default About;
+  
---
+++ src/pages/support/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Support: React.FC = () => {
+    return (
+      <div id="Support">
+        
+      </div>
+    );
+  }
+  
+  export default Support;
+  
