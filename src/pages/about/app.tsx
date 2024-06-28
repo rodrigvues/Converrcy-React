9a88ead43ab9cc45101abbca26b1210fc2d06ef7
Vitor Rodrigues <vitor.rodriguess3112@gmail.com>
2024-06-27 23:19:43 -0300

    starting about

--- src/pages/about/app.tsx
+++ src/pages/about/app.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import GlobalStyle from '../../GlobalStyle';
+import Navbar from '../../components/Navbar';
 
 const About: React.FC = () => {
     return (
       <div id="About">
-        
+        < GlobalStyle />
+        < Navbar />
       </div>
     );
   }
