de6b95e89064e439fec9760a5eebb3524d14abca
Vitor Rodrigues <vitor.rodriguess3112@gmail.com>
2024-06-29 23:57:20 -0300

    start of about and support pages

--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import Navbar from '../../components/Navbar';
+import { AboutContainer, Section, SectionTitle, Paragraph } from './styles';
 
 const About: React.FC = () => {
   return (
     <div>
       <Navbar />
-      <h1>About Page</h1>
-      <p>This is the about page.</p>
+      <AboutContainer>
+      <Section>
+        <SectionTitle>About Converrcy</SectionTitle>
+        <Paragraph>
+          Converrcy is a React project created by Vitor Rodrigues. It serves as an online currency converter, designed to provide users with real-time exchange rates for up to 32 different currencies. Whether you're traveling, managing your finances, or simply curious about exchange rates, Converrcy offers a reliable and easy-to-use solution.
+        </Paragraph>
+      </Section>
+      <Section>
+        <SectionTitle>Features</SectionTitle>
+        <Paragraph>
+          Converrcy currently supports the following features:
+        </Paragraph>
+        <Paragraph>
+          - Conversion of up to 32 different currencies.<br />
+          - Real-time exchange rates to ensure accuracy.<br />
+        </Paragraph>
+      </Section>
+    </AboutContainer>
     </div>
   );
 };
--- src/pages/support/support.tsx
+++ src/pages/support/support.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import Navbar from '../../components/Navbar';
+import { SupportContainer, Section, SectionTitle, Paragraph } from './styles';
 
 const Support: React.FC = () => {
   return (
     <div>
       <Navbar />
-      <h1>Support Page</h1>
-      <p>This is the support page.</p>
+      <SupportContainer>
+      <Section>
+        <SectionTitle>Support</SectionTitle>
+        <Paragraph>
+          If you need any assistance or have any questions about Converrcy, please don't hesitate to contact for support. We are here to help you with any issues or inquiries you may have.
+        </Paragraph>
+        <Paragraph>
+          For support, please contact at: <a href="mailto:rodrigvues@gmail.com">rodrigvues@gmail.com</a>
+        </Paragraph>
+        <Paragraph>
+          We strive to respond to all support requests as quickly as possible and appreciate your patience.
+        </Paragraph>
+      </Section>
+    </SupportContainer>
     </div>
   );
 };
